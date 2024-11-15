36d894a25e7f7a027b064c0fb732ae335e396366
movb0 <bbamunuarachchi@gmail.com>
2024-11-15 19:51:45 +0530

    updated course carousel and video section files

--- LearningKid_CourseManagementSystem-main/lms-frontend/src/components/User/CourseCarousel.jsx
+++ LearningKid_CourseManagementSystem-main/lms-frontend/src/components/User/CourseCarousel.jsx
@@ -1,30 +1,109 @@
-// src/components/CourseCarousel.js
-import React from 'react';
 import CourseCard from './CourseCard';
-import courseImage from '../../assets/images/courses-01.jpg'
-import authorImage from '../../assets/images/author-01.png'
-import courseImage2 from '../../assets/images/courses-02.jpg'
-import authorImage2 from '../../assets/images/author-02.png'
+
+import courseImage from '../../assets/images/mathematics.jpg';
+import authorImage from '../../assets/images/author-01.png';
+import courseImage2 from '../../assets/images/science.jpg';
+import authorImage2 from '../../assets/images/author-02.png';
+import courseImage3 from '../../assets/images/english_lang.jpg';
+import courseImage4 from '../../assets/images/Sinhala_lang.PNG';
+import courseImage5 from '../../assets/images/history.jpg';
+import courseImage6 from '../../assets/images/geography.jpg';
+import courseImage7 from '../../assets/images/business.jpg';
+import courseImage8 from '../../assets/images/buddhism.jpg';
+import courseImage9 from '../../assets/images/it.jpg';
+import courseImage10 from '../../assets/images/agriculture.jpg';
+import courseImage11 from '../../assets/images/civics.jpg';
 import './CourseCarousel.css'
 
 const courses = [
   {
-    title: 'Digital Marketing',
-    description: 'You can get free images and videos for your websites by visiting Unsplash, Pixabay, and Pexels.',
+    title: 'Mathematics',
+    description: 'Gain essential mathematical skills needed for academic and daily life applications.',
     image: courseImage,
     authorImage: authorImage,
-    authorName: 'Author 1',
-    isFree: false,
-  },
-  {
-    title: 'Business World',
-    description: 'Quisque cursus augue ut velit dictum, quis volutpat enim blandit.',
-    image: courseImage2,
-    authorImage: authorImage2,
-    authorName: 'Author 2',
+    authorName: 'Math Department',
     isFree: true,
   },
-  
+  {
+    title: 'Science',
+    description: 'Explore fundamental concepts in Physics, Chemistry, and Biology.',
+    image: courseImage2,
+    authorImage: authorImage2,
+    authorName: 'Science Department',
+    isFree: true,
+  },
+  {
+    title: 'English Language',
+    description: 'Develop your proficiency in reading, writing, and speaking English.',
+    image: courseImage3,
+    authorImage: authorImage,
+    authorName: 'English Department',
+    isFree: true,
+  },
+  {
+    title: 'Sinhala Language',
+    description: 'Enhance your skills in Sinhala grammar, vocabulary, and comprehension.',
+    image: courseImage4,
+    authorImage: authorImage2,
+    authorName: 'Sinhala Department',
+    isFree: true,
+  },
+  {
+    title: 'History',
+    description: 'Understand the key historical events and developments that shaped the world.',
+    image: courseImage5,
+    authorImage: authorImage,
+    authorName: 'History Department',
+    isFree: true,
+  },
+  {
+    title: 'Geography',
+    description: 'Study the physical features of the Earth and human geography.',
+    image: courseImage6,
+    authorImage: authorImage,
+    authorName: 'Geography Department',
+    isFree: true,
+  },
+  {
+    title: 'Business',
+    description: 'Learn business principles and management skills.',
+    image: courseImage7,
+    authorImage: authorImage,
+    authorName: 'Business Department',
+    isFree: true,
+  },
+  {
+    title: 'Buddhism',
+    description: 'Learn the teachings and practices of Buddhism.',
+    image: courseImage8,
+    authorImage: authorImage2,
+    authorName: 'Buddhism Department',
+    isFree: true,
+  },
+  {
+    title: 'Information Technology',
+    description: 'Study essential IT concepts and skills.',
+    image: courseImage9,
+    authorImage: authorImage,
+    authorName: 'IT Department',
+    isFree: true,
+  },
+  {
+    title: 'Agriculture',
+    description: 'Learn the basics of farming and agriculture.',
+    image: courseImage10,
+    authorImage: authorImage2,
+    authorName: 'Agriculture Department',
+    isFree: true,
+  },
+  {
+    title: 'Civics',
+    description: 'Learn about your rights, duties, and responsibilities as a citizen.',
+    image: courseImage11,
+    authorImage: authorImage,
+    authorName: 'Civics Department',
+    isFree: true,
+  },
 ];
 
 const CourseCarousel = () => {
@@ -34,7 +113,7 @@ const CourseCarousel = () => {
         <div className="row">
           <div className="col-md-12">
             <div className="section-heading">
-              <h2>Choose Your Course</h2>
+              <h2>Choose Your O/L Course</h2>
             </div>
           </div>
           <div className="owl-carousel owl-theme">
--- LearningKid_CourseManagementSystem-main/lms-frontend/src/components/User/VideoSection.jsx
+++ LearningKid_CourseManagementSystem-main/lms-frontend/src/components/User/VideoSection.jsx
@@ -1,32 +1,31 @@
 // src/components/VideoSection.js
+
 import React from 'react';
 import './VideoSection.css';
-import mainthumb from "../../assets/images/main-thumb.png"
+import mainthumb from "../../assets/images/main-thumb.png";
 
 const VideoSection = () => {
   return (
-    <section className="section video" data-section="section5">
+    <section className="video-section">
       <div className="container">
-        <div className="row">
-          <div className="col-md-6 align-self-center">
-            <div className="left-content">
-              <h4>Watch the video to learn more <em>about Learning Kidz</em></h4>
-              <div className="main-button"><a rel="nofollow" href="https://fb.com/templatemo" target="_parent">External URL</a></div>
-            </div>
-          </div>
-          <div className="col-md-6">
-            <article className="video-item">
-              <div className="video-caption">
-                <h4>Course for you</h4>
-              </div>
-              <figure>
-                <a href="" className="play">
-                  <img src={mainthumb} alt="Video Thumbnail" />
-                </a>
-              </figure>
-            </article>
+        {/* Section Heading */}
+        <div className="video-description">
+          <h4>Watch the video to learn more <em>about Learning Kidz</em></h4>
+          <div className="main-button">
+            <a href="https://fb.com/templatemo" target="_blank" rel="noopener noreferrer">
+              External URL
+            </a>
           </div>
         </div>
+        
+        {/* Video Thumbnail */}
+        <div className="video-item">
+          <figure>
+            <img src={mainthumb} alt="Video Thumbnail" />
+          </figure>
+          <h4>Course for you</h4>
+        </div>
+        
       </div>
     </section>
   );
